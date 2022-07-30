7400b1f11198fc417e126c9997b35dead7a5159e
putrya1969 <putrya1969@gmail.com>
2022-07-30 23:54:55 +0300

    added new branch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("How are you?");
+Console.WriteLine("Just a make new branch");
 Console.ReadKey();
